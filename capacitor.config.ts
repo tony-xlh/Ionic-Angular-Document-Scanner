4763972c16c5c83edaefae8a225e96041d0dc451
xulihang <xulihanghai@163.com>
2022-11-17 14:50:58 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Document Scanner',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
