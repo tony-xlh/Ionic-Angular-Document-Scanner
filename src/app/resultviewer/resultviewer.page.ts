7a6fc5c4a5d519592f2726d51ca19d9428c81e99
xulihang <xulihanghai@163.com>
2022-11-18 11:32:41 +0800

    display the normalized image in the result viewer

--- src/app/resultviewer/resultviewer.page.ts
+++ src/app/resultviewer/resultviewer.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DetectedQuadResult } from 'dynamsoft-document-normalizer';
+import { DocumentNormalizer } from 'capacitor-plugin-dynamsoft-document-normalizer';
 
 @Component({
   selector: 'app-resultviewer',
@@ -7,7 +9,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./resultviewer.page.scss'],
 })
 export class ResultviewerPage implements OnInit {
-
+  dataURL:string = "";
+  normalizedImageDataURL:string = "";
+  private detectedQuadResult:DetectedQuadResult|undefined;
   constructor(private router: Router) { }
 
   ngOnInit() {
@@ -15,6 +19,18 @@ export class ResultviewerPage implements OnInit {
     if (navigation) {
       const routeState = navigation.extras.state;
       console.log(routeState);
+      if (routeState) {
+        this.dataURL = routeState["dataURL"];  
+        this.detectedQuadResult = routeState["detectedQuadResult"];
+        this.normalize();
+      }
+    }
+  }
+
+  async normalize() {
+    if (this.detectedQuadResult) {
+      let normalizedImageResult = await DocumentNormalizer.normalize({source:this.dataURL,quad:this.detectedQuadResult.location});
+      this.normalizedImageDataURL = normalizedImageResult.result.data;
     }
   }
 
